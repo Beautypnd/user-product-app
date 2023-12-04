adae715cbe62b2c28f283436370bf8e74d16ed79
Beautypnd <99247882+Beautypnd@users.noreply.github.com>
2023-12-04 22:16:15 +0530

    Delete merchant-product-app directory

--- merchant-product-app/src/main/java/org/jsp/merchantproductapp/repository/MerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package org.jsp.merchantproductapp.repository;
-
-import org.jsp.merchantproductapp.dto.Merchant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MerchantRepository extends JpaRepository<Merchant, Integer> {
-
-}
-
